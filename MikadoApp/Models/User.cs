dc773a578f400ae24526bd222c56c95b94d70c6b
Pierre Fournier <pierre.fournier@ynov.com>
2020-02-15 14:39:15 +0100

    begin the project, class User

---
+++ MikadoApp/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MikadoApp.Models
+{
+    public class User
+    {
+        public User()
+        {
+        }
+    }
+}
---
+++ MikadoApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MikadoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

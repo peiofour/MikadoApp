55855769b3df251d4f6a5bf11b09adf1873fdd88
Pierre Fournier <pierre.fournier@ynov.com>
2020-02-15 15:31:04 +0100

    adding media model

---
+++ MikadoApp/Models/Media.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace MikadoApp.Models
+{
+    public class Media
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public MediaType Type { get; set; }
+
+    }
+
+    public enum MediaType
+    {
+        Music, Movie, Book
+    }
+}
